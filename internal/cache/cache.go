99722466357ff1698f2df2108fd49e1f468498ef
Nick Rinaldi <nickrinaldi@nicks-mbp.mynetworksettings.com>
2023-10-20 02:35:17 -0400

    added business logic fun + dockerfile

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,1 +1,9 @@
 // Implementation of the URL cache
+
+// add func to create a new cache using sync.Map
+// make new cache using sync.map
+// using a struct of url data
+// add func to check if url is in cache
+// if url in cache, return message or code
+// add func to add url and content to catch
+// if url not in cache, add url and content
--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -1,1 +1,4 @@
 // Implementation of URL fetching
+
+// func fetches content of a url and returns it
+// what kind of content would I want?
